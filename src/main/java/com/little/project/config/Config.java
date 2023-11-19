fc89ce69131b55195917b433ab1e50d780a01168
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-19 20:55:04 -0300

    Students was save in the database

--- src/main/java/com/little/project/config/Config.java
+++ src/main/java/com/little/project/config/Config.java
@@ -1,33 +1,70 @@
 package com.little.project.config;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.little.project.entities.Student;
+import com.little.project.repositories.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-// import com.little.project.repositories.StudentRepository;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @Profile("test")
 public class Config implements CommandLineRunner {
 
-    // @Autowired
-    // private StudentRepository studentRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
+    @Value("${api.url}")
+    private String apiUrl;
+    
     @Override
-    public void run(String... args) throws Exception {
-            // URL da qual você quer recuperar os dados
-        String apiUrl = "https://rmi6vdpsq8.execute-api.us-east-2.amazonaws.com/msAluno";
+    public void run(String... args) throws Exception {        
 
-        // Cria uma instância do RestTemplate
         RestTemplate restTemplate = new RestTemplate();
 
-        // Faz a chamada à URL e obtém a resposta como String
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+        List<Student> students = new ArrayList<>();
 
-        // Imprime o conteúdo da resposta no console
-        System.out.println("Resposta da URL:\n" + responseEntity.getBody());
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                String responseBody = responseEntity.getBody();
+        
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(responseBody);
+                
+                for (JsonNode node : jsonNode) {                    
+                    JsonNode idNode = node.get("id");
+                    long id = (idNode != null && !idNode.isNull()) ? idNode.asLong() : 0;
+                    
+                    JsonNode nameNode = node.get("nome");
+                    String name = (nameNode != null && !nameNode.isNull()) ? nameNode.asText() : "";
+                    
+                    JsonNode cursoNode = node.get("curso");
+                    String curso = (cursoNode != null && !cursoNode.isNull()) ? cursoNode.asText() : "";
+                    
+                    Student student = new Student(id, name, curso);
+                    students.add(student);
+                }
+                studentRepository.saveAll(students);
+                students.forEach(s -> System.out.println("ID: " + s.getId() + ", Nome: " + s.getName() + ", Curso: " + s.getCourse()));
+            } else {
+                System.out.println("Falha ao obter dados da URL. Código de resposta: " + responseEntity.getStatusCode());
+            }
+        } catch (HttpClientErrorException.NotFound e) {            
+            System.out.println("Recurso não encontrado. Mensagem: " + e.getMessage());
+        } catch (Exception e) {            
+            System.out.println("Ocorreu um erro. Mensagem: " + e.getMessage());
+        }
     }
 }
